f5d004cd16d23acbff48bf78e8723542f7ed5963
Theo <143622653+Speender@users.noreply.github.com>
2023-11-24 01:49:30 +0800

    Update project53.c

--- project53.c
+++ project53.c
@@ -1,32 +1,21 @@
 #include <stdio.h>
+void scores(int *score1, int *score2, int *score3); //Function to read three test scores
+char calculateGrade(int score1, int score2, int score3); //Function to calculate the grade
+void printResults(char grade); //function to print the final grade
 
-// Function to read three test scores
-void scores(int *score1, int *score2, int *score3);
-
-// Function to calculate the grade
-char calculateGrade(int score1, int score2, int score3);
-
-// Function to print the final grade
-void printResults(char grade);
 int main() 
 {
     int score1, score2, score3;
-
-    // Call the function to get test scores
-    scores(&score1, &score2, &score3);
-
-    // Call the function to determine the grade
-    char grade = calculateGrade(score1, score2, score3);
-
-    // Call the function to display the final grade
-    printResults(grade);
+    scores(&score1, &score2, &score3); //call the function to get test scores
+    char grade = calculateGrade(score1, score2, score3); //Call the function to determine the grade
+    printResults(grade); //call the function to display the final grade
 
     return 0;
 }
 
 void scores(int *score1, int *score2, int *score3) 
 {
-    printf("Enter the first test score (between 0 and 100): ");
+    printf("Enter the first test score (between 0 and 100): "); //user inputes
     scanf("%d", score1);
 
     printf("Enter the second test score (between 0 and 100): ");
@@ -38,8 +27,9 @@ void scores(int *score1, int *score2, int *score3)
 
 char calculateGrade(int score1, int score2, int score3) 
 {
+    //process
     int averageScore = (score1 + score2 + score3) / 3;
-    int conditionC = (score2 + score3) / 2; // This is for the condition 'C' "if checks average of the second and third scores. if the average of the two is greater than 70, the grade isc; otherwise, it is D"
+    int conditionC = (score2 + score3) / 2; //This is for the condition 'C' "if checks average of the second and third scores. if the average of the two is greater than 70, the grade isc; otherwise, it is D"
 
     if (averageScore >= 90)
     {
@@ -49,7 +39,7 @@ char calculateGrade(int score1, int score2, int score3)
     {
         return 'B';
     }
-    else if (score3 > 90) // This is for the condition 'B' "it checks the third score. If the third score is more than 90%, the grade is A; otherwise, the grade is B."
+    else if (score3 > 90) //This is for the condition 'B' "it checks the third score. If the third score is more than 90%, the grade is A; otherwise, the grade is B."
     {
         return 'A';
     }
@@ -69,5 +59,6 @@ char calculateGrade(int score1, int score2, int score3)
 
 void printResults(char grade)
 {
+    //output
     printf("The student's grade is: %c\n", grade);
 }
